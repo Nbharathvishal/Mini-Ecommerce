f3d04f84eb65288a2eec1eeb3d5101d3fa327398
Nbharathvishal <vishalbharath566@gmail.com>
2026-01-10 22:45:42 +0530

    small change in cart and env files

--- backend/app.js
+++ backend/app.js
@@ -4,18 +4,19 @@ const dotenv = require("dotenv");
 const path = require("path");
 const cors = require("cors");
 
-dotenv.config({ path: path.join(__dirname, "config", "config.env") });
+dotenv.config();
 
-app.use(express.json()); 
+app.use(express.json());
 app.use(cors());
 
 const products = require("./routes/product");
 const orders = require("./routes/order");
 
 const connectDatabase = require("./config/database");
-connectDatabase(); 
+connectDatabase();
 
-app.use(express.json())
+console.log(process.env.PORT);
+app.use(express.json());
 
 app.use("/api/v1", products);
 app.use("/api/v1", orders);
@@ -25,3 +26,4 @@ app.listen(process.env.PORT, () => {
     `Server listening to port ${process.env.PORT} in ${process.env.NODE_ENV}`
   );
 });
+
